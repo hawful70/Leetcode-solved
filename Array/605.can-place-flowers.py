edb82c6c38a8cbbd6fb08d6efcbb23439a3915bc
hawful70 <quangvinh050293@gmail.com>
2023-09-03 21:31:47 +0700

    clean code 605

--- Array/605.can-place-flowers.py
+++ Array/605.can-place-flowers.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         lenFlowbed = len(flowerbed)
@@ -8,27 +7,14 @@ class Solution:
         if n == 0:
             return True
 
-        if lenFlowbed == 1 and flowerbed[0] == 1:
-            return False
-        if lenFlowbed == 1 and flowerbed[0] == 0:
-            return True
-
         for i in range(lenFlowbed):
-            if n == 0:
-                break
-            
-            if i == 0 and flowerbed[i + 1] == 0 and flowerbed[i] == 0:
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == lenFlowbed - 1 or flowerbed[i + 1] == 0):
                 flowerbed[i] = 1
                 n -= 1
-            elif i == lenFlowbed - 1 and flowerbed[i - 1] == 0 and flowerbed[i] == 0:
-                flowerbed[i] = 1
-                n -= 1
-            elif flowerbed[i] == 0 and flowerbed[i - 1] == 0 and flowerbed[i + 1] == 0:
-                flowerbed[i] = 1
-                n -= 1
-            
-            
-        return n == 0
+                if n == 0:
+                    return True
+        return False
+        
     
 solution = Solution()
 print(solution.canPlaceFlowers([1,0,0,0,1,0,0], 2))
