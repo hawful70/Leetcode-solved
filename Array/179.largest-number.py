833abebd5dfc9281d7b70345d828a4000d9ea9d9
hawful70 <quangvinh050293@gmail.com>
2023-09-24 11:37:03 +0700

    clean 179

--- Array/179.largest-number.py
+++ Array/179.largest-number.py
@@ -1,31 +1,47 @@
+import random
 from typing import List
 
 
 class Solution:
-    def largestNumber(self, nums: List[int]) -> str:
+    def compare(self, num1, num2):
+        return str(num1) + str(num2) > str(num2) + str(num1)
+    
+    def selectionSort(self, nums: List[int]):
         n = len(nums)
-        if n == 1:
-            return "".join(map(str, nums))
-        
         currentIndex = 0
-        operation = 0
         while currentIndex < n:
             maxIndex = currentIndex
             for i in range(currentIndex + 1, n):
-                strMaxElement = str(nums[maxIndex])
-                strCurrentElement = str(nums[i])
-                if int(strMaxElement + strCurrentElement) < int(strCurrentElement + strMaxElement):
+                if not self.compare(nums[maxIndex], nums[i]):
                     maxIndex = i
                     
-            
             nums[currentIndex], nums[maxIndex] = nums[maxIndex], nums[currentIndex]
-            if nums[currentIndex] == 0:
-                operation += 1
             currentIndex += 1
 
-        if operation == len(nums):
-            return str(nums[0])
-        return "".join(map(str, nums))
+        return str(int("".join(map(str, nums))))
+    
+    def quickSort(self, nums, l, r):
+        if l >= r:
+            return 
+        pos = self.partition(nums, l, r)
+        self.quickSort(nums, l, pos-1)
+        self.quickSort(nums, pos+1, r)
+    
+    def partition(self, nums, l, r):
+        max = l
+        while l < r:
+            if self.compare(nums[l], nums[r]):
+                nums[l], nums[max] = nums[max], nums[l]
+                max += 1
+            l += 1
+        nums[max], nums[r] = nums[r], nums[max]
+        return max
+
+    def largestNumber(self, nums: List[int]) -> str:
+        # ans = self.selectionSort(nums)
+        self.quickSort(nums, 0, len(nums)-1)
+        return str(int("".join(map(str, nums)))) 
+
     
 solution = Solution()
-print(solution.largestNumber([0,9,8,7,6,5,4,3,2,1]))
+print(solution.largestNumber([3,30,34,5,9]))
