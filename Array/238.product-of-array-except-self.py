358d9c3d7496af49b477d49fd0027a0d043689b5
hawful70 <quangvinh050293@gmail.com>
2023-12-08 15:34:13 +0700

    clean 238, implement 215

--- Array/238.product-of-array-except-self.py
+++ Array/238.product-of-array-except-self.py
@@ -24,5 +24,19 @@ class Solution:
             ans.append(left_array[i] * right_array[i])
 
         return ans
+    
+    def productExceptSelfV2(self, nums: List[int]) -> List[int]:
+        length = len(nums)
+        ans = [1] * length
+        pre = 1
+        post = 1
+
+        for i in range(len(nums)):
+            ans[i] *= pre
+            pre *= nums[i]
+            ans[length - i - 1] *= post
+            post *= nums[length - i - 1]
+
+        return ans
 solution = Solution()
 print(solution.productExceptSelf([1,2,3,4]))
